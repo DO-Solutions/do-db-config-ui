ea1d7c7174d9c5154b8e404673ac349fa32bfddf
Jack Pearce <16779171+jkpe@users.noreply.github.com>
2025-02-17 22:48:38 +0000

    Remove unused js

--- src/config/index.js
+++
@@ -1,6 +0,0 @@
-export * from './mysql';
-export * from './postgres';
-export * from './redis';
-export * from './mongo';
-export * from './kafka';
-export * from './opensearch';
--- src/pages/index.js
+++
@@ -1,5 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import DatabaseConfigApp from '../components/DatabaseConfigApp';
-export default function IndexPage() {
-    return _jsx(DatabaseConfigApp, {});
-}
